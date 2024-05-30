12029a364f5f8c9d85aaab7767b64039d73e21e5
EmilioCarreiraOWI <221350@virtualwindow.co.za>
2024-05-30 12:50:29 +0200

    not finnised

--- screens/AddScreen.jsx
+++ screens/AddScreen.jsx
@@ -1,9 +1,35 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, TextInput, Button, Image } from 'react-native'
 import React, { useState } from 'react'
+import * as ImagePicker from 'expo-image-picker';
+import { handleUploadLoadOfImage } from '../services/BucketService';
+import { createNewBucketMemory } from '../services/BucketService';
 
 const AddScreen = () => {
 
     const [title, setTitle] = useState('')
+    const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const uploadImage = async() => {
+    handleUploadLoadOfImage(image, title);
+  }
+
+
 
 
   return (
@@ -18,8 +44,10 @@ const AddScreen = () => {
         />
 
         {/* TODO: Upload Image */}
+        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        {image && <Image source={{ uri: image }} style={styles.image} />}
 
-        <TouchableOpacity style={styles.button} >
+        <TouchableOpacity style={styles.button} onPress={uploadImage}>
             <Text style={styles.buttonText}>Add Memory</Text>
         </TouchableOpacity>
         
@@ -49,4 +77,8 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: 'white'
     },
+    image: {
+        width: 200,
+        height: 200,
+      },
 })
--- services/BucketService.js
+++ services/BucketService.js
@@ -1,1 +1,33 @@
+import { getDownloadURL, uploadBytes, ref } from "firebase/storage";
+import { storage } from "../firebase";
+import { db } from "../firebase";
+import { createNewBucketMemory } from "./dbServive";
+
 // TODO: Upload Image to Buckets
+export const handleUploadLoadOfImage = async (uri, fileName) => {
+
+    const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            resolve(xhr.response);
+        }
+        xhr.onerror = function () {
+            console.log(e);
+            reject(new TypeError("network request failed"));
+        }
+        xhr.responseType = 'blob';
+        xhr.open("GET", uri, true);
+        xhr.send(null);
+    })
+
+    const imageRef = ref(storage, fileName);
+
+    const uploadResult = await uploadBytes(imageRef, blob);
+
+    blob.close();
+
+    const imageUrl = await getDownloadURL(imageRef);
+    console.log("image URL: ", imageUrl);
+
+    createNewBucketMemory(fileName, imageUrl)
+}
